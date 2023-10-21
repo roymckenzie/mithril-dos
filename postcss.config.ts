a7f5b60466a77ebaddb7a1c5007933335cc76315
Roy <roy@roymckenzie.me>
2023-10-21 01:57:20 -0500

    - Reverting PostCSS config to module.exports

--- postcss.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "postcss-load-config";
-
-export default {
-  plugins: {
-    "tailwindcss/nesting": {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-} satisfies Config;
